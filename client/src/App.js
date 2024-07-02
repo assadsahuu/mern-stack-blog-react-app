4b27d9605a6a80a957cde4cb18b0700cd1e6bb3a
assadsahuu <assadullahsahu@yahoo.com>
2024-07-02 22:23:55 +0500

    pages and routes set and header component

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import {BrowserRouter,Routes,Route} from  'react-router-dom'
+import './App.css';
+import About from './pages/About'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Header from './components/Header';
+
+function App() {
+  return (
+    <BrowserRouter>
+<Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='About' element={<About/>}/>
+        <Route path='Dashboard' element={<Dashboard/>}/>
+        <Route path='Projects' element={<Projects/>}/>
+        <Route path='SignIn' element={<SignIn/>}/>
+        <Route path='SignUp' element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
