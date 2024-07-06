85a80f8ee50d98ea39846788716a79060a7841ae
assadsahuu <assadullahsahu@yahoo.com>
2024-07-06 18:47:59 +0500

    setting up test api

--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
-const express=require('express')
-const app=express();
-const mongoose=require('mongoose')
-const dotenv=require('dotenv')
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import express from 'express';
+const app = express();
+import router from './routes/user.route.js'
 dotenv.config();
 mongoose.connect(process.env.mongooo)
-.then(()=>{
-    console.log('Mongodb is connected')
-}).catch(err=>{
-    console.log(err)
+    .then(() => {
+        console.log('Mongodb is connected')
+    }).catch(err => {
+        console.log(err)
+    })
+app.listen(3000, () => {
+    console.log('server is running on port 3000 !')
 })
-app.listen(3000,()=>{
-console.log('server is running on port 3000 !')
-})
+
+
+app.use('/api/user',router);
