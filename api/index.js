d5f3805746468c2d9d5e840a9c525c166d41acbc
assadsahuu <assadullahsahu@yahoo.com>
2024-07-06 15:34:39 +0500

    MongoDb Altas connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
-const express=require("express")
+const express=require('express')
 const app=express();
-
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config();
+mongoose.connect(process.env.mongooo)
+.then(()=>{
+    console.log('Mongodb is connected')
+}).catch(err=>{
+    console.log(err)
+})
 app.listen(3000,()=>{
 console.log('server is running on port 3000 !')
 })
