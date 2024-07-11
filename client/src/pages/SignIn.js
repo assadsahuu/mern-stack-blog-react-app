e6c31d3be7c05ad2d095a414521f259baed322a6
assadsahuu <assadullahsahu@yahoo.com>
2024-07-11 17:27:37 +0500

    add redux toolkit

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,7 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Label, TextInput, Button } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Label, TextInput, Button, Alert, Spinner } from 'flowbite-react'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
+
+
+
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { loading, error: errorMessage } = useSelector(state => state.user);
+
+
+
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure('Please fill out all fields'));
+    }
+    try {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+      }
+     
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate('/');
+      }
+    } catch (error) {
+      dispatch(signInFailure(error));
+    }
+  };
+
+
+
   return (
 
     <div className='min-h-screen mt-20 '>
@@ -23,26 +69,40 @@ export default function SignIn() {
         </div>
         {/*right */}
         <div className="flex-1">
-          <form className='gap-4 flex flex-col'>
-            
+          <form className='gap-4 flex flex-col' onSubmit={handleSubmit}>
             <div >
               <Label value='Email'></Label>
-              <TextInput type='email' placeholder='Name@company.com' id='email' />
+              <TextInput autoComplete='email' type='email' placeholder='Name@company.com' id='email' onChange={handleChange} />
             </div>
             <div >
               <Label value='Password'></Label>
-              <TextInput autoComplete='password' type='password' placeholder='Password' id='password' />
+              <TextInput autoComplete='new-password' type='password' placeholder='********' id='password' onChange={handleChange} />
             </div>
-           
-              <Button className='sm' gradientDuoTone='purpleToBlue' type='submit' pill outline>SignIn</Button>
-            
+
+            <Button className='sm' gradientDuoTone='purpleToBlue' type='submit'
+              disabled={loading} pill outline>
+              {
+                loading ? (
+                  <>
+                    <Spinner size='sm' />
+                    <span className='pl-3'>Loading...</span>
+                  </>
+                ) : 'Sign In'
+              }
+            </Button>
+
           </form>
           <div className='mt-5 flex text-sm gap-2'>
             <span>
               Don't Have an account?
             </span>
-            <Link  to='/SignUp' className='text-blue-500' >     Sign UP </Link>
+            <Link to='/SignUn' className='text-blue-500' >     Sign Up </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5 " color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
