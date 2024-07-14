fa244297621eff0c809e6543d02ec3b686786b2a
assadsahuu <assadullahsahu@yahoo.com>
2024-07-14 13:45:46 +0500

    adding themeslice and copmlete dark mood functionality

--- client/src/components/OAuth.js
+++ client/src/components/OAuth.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button } from 'flowbite-react'
 import { AiFillGoogleCircle } from 'react-icons/ai'
-import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup, getAuth} from 'firebase/auth'
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux'
 import { signInSuccess } from '../redux/user/userSlice';
--- client/src/index.js
+++ client/src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store,persistor } from './redux/store';
+import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
-
+import ThemeProvider from './components/themeProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<PersistGate persistor={persistor}>
-<Provider store={store}>
-    <App />
-  </Provider>
-</PersistGate>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </PersistGate>
 );
 
 // If you want to start measuring performance in your app, pass a function
